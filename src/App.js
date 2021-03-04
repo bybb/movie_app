543d8548812e8c50a5e50a9a660b24da73f24f91
pkyy <71049179+pkyy@users.noreply.github.com>
2021-03-05 00:26:18 +0900

    finish

--- src/App.js
+++ src/App.js
@@ -13,23 +13,24 @@ class App extends React.Component {
       data: {
         data: { movies }
       }
-    } = await axios.get("http://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
     this.setState({ movies, isLoading: false });
-  }
+  };
   componentDidMount() {
     this.getMovies();
-
   }
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <section class="container">
+      <section className="container">
         {isLoading ? (
-          <div class="loader">
-            <span class="loader_text">Loading...</span>
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
           </div>
         ) : (
-            <div class="movies">
+            <div className="movies">
               {movies.map(movie => (
                 <Movie
                   key={movie.id}
@@ -38,10 +39,10 @@ class App extends React.Component {
                   title={movie.title}
                   summary={movie.summary}
                   poster={movie.medium_cover_image}
+                  genres={movie.genres}
                 />
               ))}
             </div>
-
           )}
       </section>
     );
